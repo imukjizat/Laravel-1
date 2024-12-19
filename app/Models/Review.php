ff34fadff53edb340f9eedd3ece45b5d6bce4127
imukjizat <ilhammukjizat255@gmail.com>
2024-12-20 02:57:24 +0700

    Menyesuikan model review

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = ['review', 'rating', 'user_id', 'movie_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function movie()
+    {
+        return $this->belongsTo(Movie::class);
+    }
 }
