28fb1a9e1106be59081c041c167a645413b4fed7
imukjizat <ilhammukjizat255@gmail.com>
2024-12-20 03:35:53 +0700

    Menyesuaikan Role Factory

--- database/factories/RoleFactory.php
+++ database/factories/RoleFactory.php
@@ -3,21 +3,15 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Ramsey\Uuid\Uuid;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Role>
- */
 class RoleFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    public function definition()
     {
         return [
-            //
+            'id' => Uuid::uuid4()->toString(),
+            'name' => $this->faker->unique()->randomElement(['Admin', 'User']),
         ];
     }
 }
