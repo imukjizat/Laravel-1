6b9ccdcf38594fd873509b088b8a9aab95a76027
imukjizat <ilhammukjizat255@gmail.com>
2024-12-20 03:21:13 +0700

    Menyesuaikan Cast Seeder

--- database/seeders/CastSeeder.php
+++ database/seeders/CastSeeder.php
@@ -2,16 +2,29 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Ramsey\Uuid\Uuid;
 
 class CastSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        DB::table('casts')->insert([
+            [
+                'id' => Uuid::uuid4()->toString(),
+                'name' => 'Leonardo DiCaprio',
+                'age' => 46,
+                'biodata' => 'An award-winning actor.',
+                'avatar' => 'leo.jpg',
+            ],
+            [
+                'id' => Uuid::uuid4()->toString(),
+                'name' => 'Bradley Cooper',
+                'age' => 47,
+                'biodata' => 'Known for his roles in hit comedies.',
+                'avatar' => 'bradley.jpg',
+            ],
+        ]);
     }
 }
