3c2ebb36004316d68ddf356d17831947e2c101f8
imukjizat <ilhammukjizat255@gmail.com>
2024-12-20 03:11:05 +0700

    Menyesuaikan Profile Seeder

--- database/seeders/ProfileSeeder.php
+++ database/seeders/ProfileSeeder.php
@@ -2,16 +2,31 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Ramsey\Uuid\Uuid;
 
 class ProfileSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        DB::table('profiles')->insert([
+            [
+                'id' => Uuid::uuid4()->toString(),
+                'biodata' => 'Lorem ipsum dolor sit amet.',
+                'age' => 30,
+                'address' => '123 Main Street',
+                'avatar' => 'avatar1.jpg',
+                'user_id' => 'UUID_USER_1', // Ganti dengan UUID dari User pertama
+            ],
+            [
+                'id' => Uuid::uuid4()->toString(),
+                'biodata' => 'Consectetur adipiscing elit.',
+                'age' => 25,
+                'address' => '456 Elm Street',
+                'avatar' => 'avatar2.jpg',
+                'user_id' => 'UUID_USER_2', // Ganti dengan UUID dari User kedua
+            ],
+        ]);
     }
 }
