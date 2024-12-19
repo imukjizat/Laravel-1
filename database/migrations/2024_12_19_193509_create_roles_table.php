9856f4268e39942e1f6b279ec7743a868a68a82c
imukjizat <ilhammukjizat255@gmail.com>
2024-12-20 02:38:39 +0700

    Menyesuikan migrasi role

--- database/migrations/2024_12_19_193509_create_roles_table.php
+++ database/migrations/2024_12_19_193509_create_roles_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->string('name', 255);
             $table->timestamps();
         });
     }
