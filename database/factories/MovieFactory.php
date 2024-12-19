f48e0b7ee8130bdb90ea945673c05993fe12eff8
imukjizat <ilhammukjizat255@gmail.com>
2024-12-20 03:38:56 +0700

    Menyesuaikan Movie Factory

--- database/factories/MovieFactory.php
+++ database/factories/MovieFactory.php
@@ -3,21 +3,20 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Ramsey\Uuid\Uuid;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Movie>
- */
 class MovieFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    public function definition()
     {
         return [
-            //
+            'id' => Uuid::uuid4()->toString(),
+            'title' => $this->faker->sentence(3),
+            'synopsis' => $this->faker->paragraph(),
+            'poster' => $this->faker->imageUrl(),
+            'year' => $this->faker->year(),
+            'available' => $this->faker->boolean(),
+            'genre_id' => null,
         ];
     }
 }
