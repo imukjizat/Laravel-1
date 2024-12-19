a2ff306a7d9d68985c515c30a7eb7fe36eb01e54
imukjizat <ilhammukjizat255@gmail.com>
2024-12-20 04:08:33 +0700

    Menyesuaikan User Seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,20 +11,24 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
+
+        $adminId = DB::table('roles')->where('name', 'Admin')->value('id');
+        $userId = DB::table('roles')->where('name', 'User')->value('id');
+
         DB::table('users')->insert([
             [
                 'id' => Uuid::uuid4()->toString(),
                 'name' => 'John Doe',
                 'email' => 'john@example.com',
                 'password' => Hash::make('password'),
-                'role_id' => 'UUID_ROLE_1', // Ganti dengan UUID dari role Admin
+                'role_id' => $adminId,
             ],
             [
                 'id' => Uuid::uuid4()->toString(),
                 'name' => 'Jane Smith',
                 'email' => 'jane@example.com',
                 'password' => Hash::make('password'),
-                'role_id' => 'UUID_ROLE_2', // Ganti dengan UUID dari role User
+                'role_id' => $userId,
             ],
         ]);
     }
