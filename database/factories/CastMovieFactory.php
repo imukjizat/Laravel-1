cb1a6f243803e79f336e485b96074dd44f744558
imukjizat <ilhammukjizat255@gmail.com>
2024-12-20 03:40:03 +0700

    Menyesuaikan Cast Factory

--- database/factories/CastMovieFactory.php
+++ database/factories/CastMovieFactory.php
@@ -5,16 +5,14 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Ramsey\Uuid\Uuid;
 
-class CastFactory extends Factory
+class CastMovieFactory extends Factory
 {
     public function definition()
     {
         return [
             'id' => Uuid::uuid4()->toString(),
-            'name' => $this->faker->name(),
-            'age' => $this->faker->numberBetween(20, 70),
-            'biodata' => $this->faker->text(200),
-            'avatar' => $this->faker->imageUrl(),
+            'movie_id' => null, // Nanti disesuaikan melalui relasi di Seeder
+            'cast_id' => null, // Nanti disesuaikan melalui relasi di Seeder
         ];
     }
 }
