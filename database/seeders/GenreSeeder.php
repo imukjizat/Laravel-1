c438ea10cb60e9f4ba9beff97342dc55dc74a200
imukjizat <ilhammukjizat255@gmail.com>
2024-12-20 03:06:56 +0700

    Menyesuaikan Genre Seeder

--- database/seeders/GenreSeeder.php
+++ database/seeders/GenreSeeder.php
@@ -2,16 +2,18 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Ramsey\Uuid\Uuid;
 
 class GenreSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        DB::table('genres')->insert([
+            ['id' => Uuid::uuid4()->toString(), 'name' => 'Action'],
+            ['id' => Uuid::uuid4()->toString(), 'name' => 'Comedy'],
+            ['id' => Uuid::uuid4()->toString(), 'name' => 'Drama'],
+        ]);
     }
 }
