38436362dbe66644fb980bad83eca970c1b11b51
imukjizat <ilhammukjizat255@gmail.com>
2024-12-20 03:10:31 +0700

    Menyesuaikan User Seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,16 +2,30 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Ramsey\Uuid\Uuid;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        DB::table('users')->insert([
+            [
+                'id' => Uuid::uuid4()->toString(),
+                'name' => 'John Doe',
+                'email' => 'john@example.com',
+                'password' => Hash::make('password'),
+                'role_id' => 'UUID_ROLE_1', // Ganti dengan UUID dari role Admin
+            ],
+            [
+                'id' => Uuid::uuid4()->toString(),
+                'name' => 'Jane Smith',
+                'email' => 'jane@example.com',
+                'password' => Hash::make('password'),
+                'role_id' => 'UUID_ROLE_2', // Ganti dengan UUID dari role User
+            ],
+        ]);
     }
 }
