14813e23e6f30188077aa4da7ccfc2c500ad94b1
imukjizat <ilhammukjizat255@gmail.com>
2024-12-20 02:51:18 +0700

    Menyesuikan migrasi review

--- database/migrations/2024_12_19_193511_create_reviews_table.php
+++ database/migrations/2024_12_19_193511_create_reviews_table.php
@@ -12,7 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('reviews', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->text('review');
+            $table->tinyInteger('rating')->unsigned()->check('rating >= 1 and rating <= 5');
+            $table->uuid('user_id');
+            $table->uuid('movie_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('movie_id')->references('id')->on('movies');
             $table->timestamps();
         });
     }
