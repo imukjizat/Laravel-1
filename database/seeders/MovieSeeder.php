e14c0936e87c5151dd835b1881c9e727de393e28
imukjizat <ilhammukjizat255@gmail.com>
2024-12-20 03:20:12 +0700

    Menyesuaikan Movie Seeder

--- database/seeders/MovieSeeder.php
+++ database/seeders/MovieSeeder.php
@@ -2,16 +2,46 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Ramsey\Uuid\Uuid;
 
 class MovieSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        $genreActionId = DB::table('genres')->where('name', 'Action')->value('id');
+        $genreComedyId = DB::table('genres')->where('name', 'Comedy')->value('id');
+        $genreDramaId = DB::table('genres')->where('name', 'Drama')->value('id');
+
+        DB::table('movies')->insert([
+            [
+                'id' => Uuid::uuid4()->toString(),
+                'title' => 'Inception',
+                'synopsis' => 'A mind-bending thriller.',
+                'poster' => 'inception.jpg',
+                'year' => '2010',
+                'available' => true,
+                'genre_id' => $genreActionId,
+            ],
+            [
+                'id' => Uuid::uuid4()->toString(),
+                'title' => 'The Hangover',
+                'synopsis' => 'A comedy about a wild bachelor party.',
+                'poster' => 'hangover.jpg',
+                'year' => '2009',
+                'available' => true,
+                'genre_id' => $genreComedyId,
+            ],
+            [
+                'id' => Uuid::uuid4()->toString(),
+                'title' => 'The Pursuit of Happyness',
+                'synopsis' => 'A drama about a struggling salesman.',
+                'poster' => 'pursuit_of_happyness.jpg',
+                'year' => '2006',
+                'available' => true,
+                'genre_id' => $genreDramaId,
+            ],
+        ]);
     }
 }
