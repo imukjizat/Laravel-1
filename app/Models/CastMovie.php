4ac2a9fba01380bf15c80a7e46c89813ee586e58
imukjizat <ilhammukjizat255@gmail.com>
2024-12-20 02:58:14 +0700

    Menyesuikan model cast movie

--- app/Models/CastMovie.php
+++ app/Models/CastMovie.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class CastMovie extends Model
 {
     use HasFactory;
+
+    protected $table = 'cast_movies'; // Nama tabel pivot
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = ['movie_id', 'cast_id'];
+
+    public function movie()
+    {
+        return $this->belongsTo(Movie::class);
+    }
+
+    public function cast()
+    {
+        return $this->belongsTo(Cast::class);
+    }
 }
