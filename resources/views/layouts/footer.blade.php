3990b2d784c3c97c1461662e7990b71b1d96de7a
imukjizat <ilhammukjizat255@gmail.com>
2024-12-20 10:50:47 +0700

    Menambahkan file layouts untuk header dan footer flowbite

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="bg-gray-800 text-gray-400 text-center p-4">
+    <p>&copy; {{ date('Y') }} MyApp. All rights reserved.</p>
+</footer>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,9 @@
+<nav class="bg-gray-800 p-4">
+    <div class="container mx-auto flex justify-between items-center">
+        <a href="/" class="text-white font-bold text-lg">MyApp</a>
+        <div>
+            <a href="{{ route('movies.index') }}" class="text-gray-300 hover:text-white mx-2">Movies</a>
+            <a href="#" class="text-gray-300 hover:text-white mx-2">About</a>
+        </div>
+    </div>
+</nav>
